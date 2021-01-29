596e00ebaef8c089c4c71a75125b835aaeaa6efe
MW <mike.waud@sparkmeter.io>
2021-01-29 00:46:15 +0000

    Meter Readings, Configs, and Config Applied via Sink [CH42947]

---
+++ lib/slim/events/meter_config_applied_event.ex
@@ -0,0 +1,18 @@
+defmodule Slim.Events.MeterConfigAppliedEvent do
+  @type t() :: %__MODULE__{}
+
+  defstruct [
+    :meter_id,
+    :last_meter_config_offset,
+    :offset,
+    :updated_at
+  ]
+
+  def key(meter_config), do: meter_config.meter_id
+
+  def offset(meter_config), do: meter_config.offset
+
+  def set_key(event, encoded_key), do: %__MODULE__{event | meter_id: encoded_key}
+
+  def set_offset(event, encoded_offset), do: %__MODULE__{event | offset: encoded_offset}
+end
--- test/slim_test.exs
+++ test/slim_test.exs
@@ -4,6 +4,7 @@ defmodule SlimTest do
 
   @cm_config_id "44445ae7-3b9e-4135-ae3f-6d3ee79ea197" |> Ecto.UUID.dump() |> elem(1)
   @customer_id "0cb15ae7-3b9e-4135-ae3f-6d3ee79ea197" |> Ecto.UUID.dump() |> elem(1)
+  @gid_mac Varint.LEB128.encode(1234)
   @meter_id "00015ae7-3b9e-4135-ae3f-6d3ee79ea197" |> Ecto.UUID.dump() |> elem(1)
   @portfolio_id "11111ae7-3b9e-4135-ae3f-6d3ee79ea197" |> Ecto.UUID.dump() |> elem(1)
   @tariff_id "0cb15ae7-3b9e-4135-ae3f-6d3ee79ea197" |> Ecto.UUID.dump() |> elem(1)
@@ -87,6 +88,69 @@ defmodule SlimTest do
 
       assert {:ok, event_type_id, key, offset, event_data} = Slim.encode_event(event)
       assert event.id == key
+      assert event.offset == offset
+      assert {:ok, event} == Slim.decode_event({event_type_id, key}, offset, event_data)
+    end
+
+    test "MeterConfigEvent" do
+      event = %Events.MeterConfigEvent{
+        meter_id: @meter_id,
+        enabled: true,
+        power_limit: 1_000.0,
+        current_limit: 65.0,
+        startup_delay: 0,
+        throttle_on_time: 10,
+        throttle_off_time: 60,
+        throttle_count_limit: 10,
+        offset: 1,
+        updated_at: 1_586_632_500
+      }
+
+      assert {:ok, event_type_id, key, offset, event_data} = Slim.encode_event(event)
+      assert event.meter_id == key
+      assert event.offset == offset
+      assert {:ok, event} == Slim.decode_event({event_type_id, key}, offset, event_data)
+    end
+
+    test "MeterConfigAppliedEvent" do
+      event = %Events.MeterConfigAppliedEvent{
+        meter_id: @meter_id,
+        last_meter_config_offset: 5,
+        offset: 3,
+        updated_at: 1_586_632_500
+      }
+
+      assert {:ok, event_type_id, key, offset, event_data} = Slim.encode_event(event)
+      assert event.meter_id == key
+      assert event.offset == offset
+      assert {:ok, event} == Slim.decode_event({event_type_id, key}, offset, event_data)
+    end
+
+    test "MeterReadingEvent" do
+      event = %Events.MeterReadingEvent{
+        apparent_power_avg: 0.0,
+        current_avg: 0.0,
+        current_max: 0.0,
+        current_min: 0.0,
+        energy: 0.0,
+        frequency: 0.0,
+        gid_mac: @gid_mac,
+        offset: 1,
+        period_end: 1_586_632_500,
+        period_start: 1_586_631_600,
+        power_factor_avg: 0.0,
+        state: "ElectricalMeterStateOn",
+        true_power_avg: 0.0,
+        true_power_inst: 0.0,
+        uptime_secs: 99_999,
+        voltage_avg: 0.0,
+        voltage_max: 0.0,
+        voltage_min: 0.0
+      }
+
+      assert {:ok, event_type_id, key, offset, event_data} = Slim.encode_event(event)
+
+      assert event.gid_mac == key
       assert event.offset == offset
       assert {:ok, event} == Slim.decode_event({event_type_id, key}, offset, event_data)
     end
