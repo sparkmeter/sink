63daef7050ae0f0978b7092bad034437a8f8a552
MW <mike.waud@sparkmeter.io>
2021-02-24 17:40:33 +0000

    use Registry to track connected clients

--- mix.exs
+++ mix.exs
@@ -15,6 +15,7 @@ defmodule Sink.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Sink.Application, []},
       extra_applications: [:logger]
     ]
   end
