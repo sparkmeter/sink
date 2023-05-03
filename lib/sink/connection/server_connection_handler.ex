e77312b302944f97a911d7ddc8476d736847af40
Benjamin Milde <benni@kobrakai.de>
2023-05-03 13:00:46 +0200

    Implement handler state

--- lib/sink/connection/server_connection_handler.ex
+++ lib/sink/connection/server_connection_handler.ex
@@ -21,20 +21,7 @@ defmodule Sink.Connection.ServerConnectionHandler do
           | :unsupported_protocol_version
           | :unsupported_application_version
 
-  @doc """
-  Return either `{:ok, %{server: Protocol.instance_id(), client: nil | Protocol.instance_id()}}` to
-  make the handshake by or `{:quarantined, reason}` to disconnect.
-  """
-
-  @callback client_configuration(client_id()) ::
-              {:ok, %{server: Protocol.instance_id(), client: nil | Protocol.instance_id()}}
-              | {:quarantined, {binary(), binary()}}
-
-  @doc """
-  Check that the client's firmware version is compatible with the server.
-  """
-  @callback supported_application_version?(client_id(), Protocol.application_version()) ::
-              boolean()
+  @type handler_state :: term
 
   @doc """
   Run implementer's authentication logic
@@ -42,30 +29,54 @@ defmodule Sink.Connection.ServerConnectionHandler do
   @callback authenticate_client(peer_cert()) :: {:ok, client_id()} | {:error, Exception.t()}
 
   @doc """
+  Return either `{:ok, %{server: Protocol.instance_id(), client: nil | Protocol.instance_id()}}` to
+  make the handshake by or `{:quarantined, reason}` to disconnect.
+  """
+
+  @callback client_configuration(client_id()) ::
+              {:ok,
+               %{
+                 server: Protocol.instance_id(),
+                 client: nil | Protocol.instance_id()
+               }, handler_state}
+              | {:quarantined, {binary(), binary()}, handler_state}
+
+  @doc """
+  Check that the client's firmware version is compatible with the server.
+  """
+  @callback supported_application_version?(
+              client_id(),
+              Protocol.application_version(),
+              handler_state
+            ) ::
+              boolean()
+
+  @doc """
   Run implementer's logic for handling a "connection response"
   """
-  @callback handle_connection_response(client_id(), connection_responses) :: :ok
+  @callback handle_connection_response(client_id(), connection_responses, handler_state) ::
+              :ok
 
   @doc """
   Run implementer's logic for handling a "ack"
   """
-  @callback handle_ack(client_id(), ack_key()) :: :ok
+  @callback handle_ack(client_id(), ack_key(), handler_state) :: :ok
 
   @doc """
   Run implementer's logic for handling a "nack"
   """
-  @callback handle_nack(client_id(), ack_key(), Protocol.nack_data()) :: :ok
+  @callback handle_nack(client_id(), ack_key(), Protocol.nack_data(), handler_state) :: :ok
 
   @doc """
   Run implementer's logic for handling a "publish" message.
 
   Should respond with either an ack or a nack with information about the nack
   """
-  @callback handle_publish(client_id(), Sink.Event.t(), message_id()) ::
+  @callback handle_publish(client_id(), Sink.Event.t(), message_id(), handler_state) ::
               :ack | {:nack, Protocol.nack_data()}
 
   @doc """
   The connection has been closed
   """
-  @callback down(client_id()) :: :ok
+  @callback down(client_id(), handler_state) :: :ok
 end
